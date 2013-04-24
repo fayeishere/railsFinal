ff67d6ef59afc6eed94fa83915aa06783c1405cf
Faye Purdum <fayepurdum@gmail.com>
2013-04-24 11:58:53 -0700

    first

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,9 @@
+class StaticController < ApplicationController
+  def about
+    render 'shared/steve'
+  end
+
+  def contact
+    render 'shared/steve'
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFinal::Application.config.secret_token = 'a2a0f75595c7d3203f4a8ddcc7e722b946a3346e20b00c5c2d235f28267f8465a5c7bac2b8d346c636c1337ceb449c881b86e98ea863b4c2b27b66c6c50a5679'
